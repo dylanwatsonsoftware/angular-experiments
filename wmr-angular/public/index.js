ad2192e23c1522d4264eb19588ca3e4bd7867e57
Dylan Watson <lotsabackscatter@gmail.com>
2021-02-07 10:09:07 +0800

    https://github.com/yjaaidi/ng-experiments/tree/wmr-angular

---
+++ wmr-angular/public/index.js
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { bootstrapModule, createComponent, createModule } from './ngx-light';
+import { ReactiveComponentModule } from '@ngrx/component';
+import { timer } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+module.hot && module.hot.accept();
+
+const Demo = createComponent({
+  selector: "mc-demo",
+  styles: [`:host { display: block; text-align: center; }`],
+  template: `
+      <img [src]="pictureUrl">
+      <div>{{ now$ | ngrxPush | date:'HH:mm:ss' }}</div>
+      <button (click)="setColor('blue')">Ahh! {{color}}</button>
+    `,
+  component: class {
+    color = "red22";
+    setColor(color) {
+      this.color = color;
+    }
+    pictureUrl = "https://marmicode.io/f3683922cdecc8b0642e4ab8f8f1d35e.gif";
+    now$ = timer(0, 1000).pipe(map(() => new Date()));
+  },
+});
+
+const App = createComponent({
+  selector: 'mc-app',
+  template: `<mc-demo></mc-demo>`,
+});
+
+const AppModule = createModule({
+  bootstrap: [App],
+  declarations: [App, Demo],
+  imports: [BrowserModule, ReactiveComponentModule],
+});
+
+bootstrapModule(AppModule);
